729ee14872598ee9f4cd0736914427985ca58e58
dursunemre <dursunemredurhan@hotmail.com>
2023-05-18 23:59:29 +0300

    get set metodları
    burada classımızda oluşturduğumuz nesnelerin değerlerine ulaşıp oynayabilmek için get set metodları ekliyoruz

--- magaza_satis_takip.py
+++ magaza_satis_takip.py
@@ -5,6 +5,30 @@ class Magaza:
         self.__satici_cinsi = satici_cinsi
         self.__satis_miktari = satis_miktari
 
+    def get_magaza_adi(self):
+        return self.__magaza_adi
+
+    def get_satici_adi(self):
+        return self.__satici_adi
+
+    def get_satici_cinsi(self):
+        return self.__satici_cinsi
+
+    def get_satis_miktari(self):
+        return self.__satis_miktari
+
+    def set_magaza_adi(self, magaza_adi):
+        self.__magaza_adi = magaza_adi
+
+    def set_satici_adi(self, satici_adi):
+        self.__satici_adi = satici_adi
+
+    def set_satici_cinsi(self, satici_cinsi):
+        self.__satici_cinsi = satici_cinsi
+
+    def set_satis_miktari(self, satis_miktari):
+        self.__satis_miktari = satis_miktari
+
 
 
 
