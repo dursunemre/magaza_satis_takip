2b519aeff8442d480f66883dd56b1b1451b24bc5
dursunemre <dursunemredurhan@hotmail.com>
2023-05-18 23:51:28 +0300

    Add files via upload

---
+++ magaza_satis_takip.py
@@ -0,0 +1,15 @@
+class Magaza:
+    def __init__(self,magaza_adi=" ",satici_adi=" ",satici_cinsi=" ",satis_miktari=" "):
+        self.__magaza_adi = magaza_adi
+        self.__satici_adi = satici_adi
+        self.__satici_cinsi = satici_cinsi
+        self.__satis_miktari = satis_miktari
+
+
+
+
+
+
+
+
+
